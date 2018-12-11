37bf00ee0c9d9afedba0dbfd262a30a23d0c00fa
RobinPorta <robin.11011996@gmail.com>
2018-12-11 12:21:22 +0100

    Update index.php

--- log/gestore/index.php
+++ log/gestore/index.php
@@ -49,7 +49,7 @@
 
 
   ?>
-  <div id="login" class="totlogin" style="margin:15px; align-text:center;">
+  <div id="login" class="totlogin" style="margin:15px; text-align:center;">
     <div id="titolologin" class="titolo">
       <label >Profilo</label>
     </div>
